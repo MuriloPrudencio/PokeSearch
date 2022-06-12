90f58c7574ee63111f60e188cd13bc04e6342dd5
Murilo Henrique Prudencio <90528471+MuriloPrudencio@users.noreply.github.com>
2022-06-12 16:17:49 -0300

    Ajustando as telas

--- js/sobre/content.js
+++ js/sobre/content.js
@@ -6,8 +6,8 @@ export function content() {
   const content = document.createElement("p");
   content.setAttribute("class", "content");
 
-  content.innerHTML = `<b>Poke</b> Search, uma plataforma feita para você, que assim como eu é apaixonado por <b>Pokemon</b>. </br></br>
-    Você lembra de como você saia da escola? </br>
+  content.innerHTML = `<b>Poke</b> Search, uma plataforma feita para você, que assim como eu, é apaixonado por <b>Pokemon</b>. </br></br>
+    Você se lembra de como saia da escola, na época? </br>
     Ansioso para chegar em casa e assistir o desenho.</br></br>
     <p>Então basta pesquisar pelo nome de seu <b>Pokemon</b> favorito, e matar a saudade.</p>
   `;
